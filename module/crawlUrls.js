210938dd180f8ec00f7e78a4cd3d32cbc61e70b6
badewa kayode <badewakayode1@gmail.com>
2018-03-26 18:05:33 +0100

    add grouped selectors

--- module/crawlUrls.js
+++ module/crawlUrls.js
@@ -14,12 +14,14 @@ function KEY_ERROR(arg1, arg2) {
 
 const getUrlOut = util.getUrlOut; //util.getUrlOut is used frequently; setting a const for its function
 
-module.exports = function(){
-    let gen, visitedLinks = [], getDomContents;
+module.exports = function () {
+    let gen, visitedLinks = [],
+        getDomContents;
     let configured = false;
     //the variables to be configured
-    let fetchSelector, fetchSelectBy, nextSelector, nextSelectBy, formatUrl, timeOut = false, group, rateLimit,
-    
+    let fetchSelector, fetchSelectBy, nextSelector, nextSelectBy, formatUrl, timeOut = false,
+        groups, _groupSet ={}, rateLimit,
+
         //set all defaultDynamicSchemas props when the variable reference is undefined
         defaultDynamicSchemas = {
             fetchSelector: undefined,
@@ -27,9 +29,9 @@ module.exports = function(){
             nextSelector: undefined,
             nextSelectBy: undefined
         };
-    
+
     let fetchFn, nextFn;
-    
+
     /**
      *
      * @param urls
@@ -40,15 +42,15 @@ module.exports = function(){
         let initialLink = [];
         let scrapedData = [];
         if (!Array.isArray(urls)) throw new Error('the property urls must be an Array');
-    
-        for (let url of urls){
-            
+
+        for (let url of urls) {
+
             if (visitedLinks.indexOf(getUrlOut(url)) === -1) { //Todo: improve the visitedLinks check
                 visitedUrls++;
-                  if (rateLimit){
-                    yield new Promise((r,x)=> setTimeout(args=>{
+                if (rateLimit) {
+                    yield new Promise((resolve, reject) => setTimeout(args => {
                         gen.next()
-                    },rateLimit))
+                    }, rateLimit))
                 }
                 visitedLinks.push(getUrlOut(url));
                 req(url);
@@ -56,20 +58,20 @@ module.exports = function(){
                 console.log(`${getUrlOut(url)} has been visited`)
             }
         }
-    
+
         if (visitedUrls == 0) { //if visited links is 0 it means it
             reject("All the links have been visited")
         }
-    
-    
+
+
         /**
          *
          * @param url
          * @private
          */
-    
+
         function req(url) {
-    
+
             request(url, function (err, response, body) {
                 visitedUrls--;
                 if (err) {
@@ -84,7 +86,7 @@ module.exports = function(){
                     }));
                     initialLink = initialLink.concat(newLink);
                 }
-    
+
                 if (visitedUrls == 0) {
                     resolve({
                         fetchedData: scrapedData,
@@ -92,46 +94,46 @@ module.exports = function(){
                     })
                 }
             });
-    
+
         }
-    
+
     }
-    
-    
+
+
     //returns the scraped data gotten from the page
     function fetchFromPage(url) {
         let selector = util.dynamicSelection(url, defaultDynamicSchemas.fetchSelector, fetchSelector);
         let selectBy = util.dynamicSelection(url, defaultDynamicSchemas.fetchSelectBy, fetchSelectBy);
-    
-        return getDomContents(selector, selectBy, fetchFn, url,group);
+
+        return getDomContents(selector, selectBy, fetchFn, url, {_groupSet,groups});
     }
-    
+
     //get the url's to crawl next
     function selectNextCrawlContent(url) {
         let selector = util.dynamicSelection(url, defaultDynamicSchemas.nextSelector, nextSelector);
         let selectBy = util.dynamicSelection(url, defaultDynamicSchemas.nextSelectBy, nextSelectBy);
         return getDomContents(selector, selectBy, nextFn, url);
     }
-    
+
     /**
      * @description configures the 
      */
     function configSelectors() {
         Object.entries(fetchSelector).forEach(selector => {
             if (selector[1]._group) {
-                if (!dom.groupObj[selector[1]._group]) {
-                    dom.groupObj[selector[1]._group] = []
+                if (!_groupSet[selector[1]._group]) {
+                    _groupSet[selector[1]._group] = {}
                 }
-                dom.groupObj[selector[1]._group].push({[selector[0]]:selector[1]._selector})
+                _groupSet[selector[1]._group][selector[0]] = selector[1]._selector
                 delete fetchSelector[selector[0]]
             }
         })
-    
+
         if (!util.keyMatch(fetchSelector, fetchSelectBy)) throw new Error(`An Error Occurred:  ${KEY_ERROR("fetchSelector","fetchSelectBy")}`);
         if (!util.keyMatch(nextSelector, nextSelectBy)) throw new Error(`An Error Occurred: ${KEY_ERROR("nextSelector","nextSelectBy")}`);
-    
+
     }
-    
+
     /**
      *
      * @param urls
@@ -139,32 +141,31 @@ module.exports = function(){
      * @return {Promise}
      */
     function initCrawl(urls, config) {
-        
+
         if (!configured) {
             let dynamicSchemas; // define the variable to hold the dynamic data
             (function (config = {}) {
-    
+
                 ({
                     fetchSelector,
                     fetchSelectBy,
-                    nextSelector,
-                    nextSelectBy,
+                    nextSelector = {},
+                    nextSelectBy = {},
                     fetchFn,
                     nextFn,
                     timeOut,
-                    group,
-                    rateLimit=0,
+                    groups = {},
+                    rateLimit = 0,
                     dynamicSchemas = {},
                     formatUrl = util.formatUrl
                 } = config);
                 configSelectors()
-                console.log(dom.groupObj)
                 Object.assign(defaultDynamicSchemas, dynamicSchemas)
-    
+
             })(config);
             configured = true
         }
-    
+
         return new Promise((resolve, reject) => {
             if (timeOut) {
                 setTimeout(() => reject(`timeout after ${timeOut}ms`), timeOut)
@@ -174,10 +175,8 @@ module.exports = function(){
             return gen
         })
     }
-    
+
     return {
         initCrawl
     }
 }
-
-
--- module/dom.js
+++ module/dom.js
@@ -3,10 +3,10 @@
  */
 let cheerio = require('cheerio');
 let util = require('./util');
-module.exports = function (content) {
 
+module.exports = function (content) {
     //errors
-    const KEY_ERROR = "the keys don't match. Make sure keys in arg1 matches keys in arg2";
+
 
     /**
      * @description returns back the Dom content from the page's body
@@ -16,15 +16,17 @@ module.exports = function (content) {
      * @param url
      * @return {*}
      */
-    function getDomContents(selector, selectBy, callback, url) {
-
+    function getDomContents(selector, selectBy, callback, url, groupSelector) {
         let $ = cheerio.load(content);
         //if the keys don't match
-        if (!util.keyMatch(selector, selectBy) && callback) return callback(Error(`An Error Occurred : \n ${KEY_ERROR}`));
-        //this doe nothing for now, since the there is always a callback passed to the getDomContents function
-        else if (!util.keyMatch(selector, selectBy)) throw  new Error(`An Error Occurred: \n ${KEY_ERROR}`);
+
         //sets the contentData based on the way the data s organized;
         let contentData = sortIndividuallyByName(selector, selectBy, $);
+
+        if (arguments.callee.caller.name == "fetchFromPage" && !util.empty(groupSelector.groups)) {
+            Object.assign(contentData, sortInGroup(groupSelector, selectBy, $))
+            // content = null;
+        }
 
         if (arguments.callee.caller.name == "selectNextCrawlContent") {
             util.relativeToAbsoluteUrl(contentData, url);
@@ -38,6 +40,48 @@ module.exports = function (content) {
         return contentData;
     }
 
+
+    function sortInGroup(selector, selectBy, $) {
+        const {
+            groups,
+            _groupSet
+        } = selector
+        const keyVal = Object.entries(_groupSet)
+        let contentData = {}
+        //loop through the groups
+        for (let key in groups) {
+            if (!contentData[key]) {
+                contentData[key] = []
+            }
+
+            $(groups[key]).map(function () {
+                //load the html content of the group to cheerio
+                try {
+                    const $$ = cheerio.load($(this).html())
+                    // group the selectors in this object
+                    let obj = {}
+
+                    for (let data in _groupSet[key]) {
+                        obj[data] = $$(_groupSet[key][data]).map(function () {
+                            if (Array.isArray(selectBy[data])) {
+                                return $$(this)[selectBy[data][0]](...selectBy[data].slice(1)) || "null"
+                            } else if (typeof $$[selectBy[data]] == 'function') {
+                                return $$(this)[selectBy[data]]() || "null";
+                            } else if (typeof $$[selectBy[data]] !== 'undefined') {
+                                return $$(this)[selectBy[data]] || "null";
+                            }
+
+                        }).get()
+                    }
+                    contentData[key].push(obj)
+                } catch (err) {
+                    console.log("group selector not found on the page")
+                }
+            }).get()
+            return contentData
+        }
+    }
+
     /**
      * @description extracts the data by the name given to the selector Object keys.
      * @param selector
@@ -48,6 +92,7 @@ module.exports = function (content) {
     function sortIndividuallyByName(selector, selectBy, $) {
         let contentData = {};
         for (let data in selector) {
+            //dont sort name that belong to a group
             contentData[data] = $(selector[data]).map(function (index, element) {
                 if (Array.isArray(selectBy[data])) {
                     return $(this)[selectBy[data][0]](...selectBy[data].slice(1)) || 'null'
@@ -62,8 +107,11 @@ module.exports = function (content) {
     }
 
 
+
+
     return {
         getDomContents
     }
 };
 
+module.exports.groupObj = {}
